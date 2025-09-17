30a2f76cb80c11338043412106b9d8a1c280d6c4
Okta Pilopa <pilopaokta@gmail.com>
2025-09-17 14:04:12 +0800

    edit layout menu edit

--- edit.php
+++ edit.php
@@ -29,6 +29,32 @@ if (isset($_POST['update'])) {
 <?php include "templates/header.php"; ?>
 <?php include "templates/navbar.php"; ?>
 <?php include "templates/sidebar.php"; ?>
-<?php include "templates/style_edit.php"; ?>
 
-</html>
+<div class="content">
+    <div class="card">
+        <div class="card-body">
+            <h2>Edit Barang</h2>
+        <form method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label>Foto Barang</label><br>
+                <?php if ($data['foto']) { ?>
+                    <img src="uploads/<?= $data['foto'] ?>" width="100" class="mb-2 rounded"><br>
+                <?php } ?>
+                <input type="file" name="foto" class="form-control" accept="image/*">
+            </div>
+            <div class="mb-3">
+                <label>Nama Barang</label>
+                <input type="text" name="nama_barang" class="form-control" value="<?= $data['nama_barang'] ?>" required>
+            </div>
+            <div class="mb-3">
+                <label>Jumlah</label>
+                <input type="number" name="jumlah" class="form-control" value="<?= $data['jumlah'] ?>" required>
+            </div>
+            <button type="submit" name="update" class="btn btn-primary">Update</button>
+            <a href="index.php" class="btn btn-secondary">Kembali</a>
+        </form>
+        </div>
+    </div>
+</div>
+<?php include "templates/footer.php"; ?>
+
